240f0e1c8df76babd7b9ec21933e4d85f6700233
Tim Sherratt <tim@discontents.com.au>
2019-03-07 12:29:19 +1100

    Update to v.2 of Trove API and python3

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
-from urllib2 import urlopen, URLError, HTTPError
-from urllib import unquote_plus, quote_plus
+import requests
+from requests.exceptions import HTTPError
+from urllib.parse import unquote_plus, quote_plus
 import re
 from flask import request, render_template
 import os
@@ -22,16 +23,13 @@ def show_api_results():
             format = 'json' if 'json' in url else 'xml'
             api_url = '{}&key={}'.format(url, TROVE_API_KEY) if '?' in url else '{}?key={}'.format(url, TROVE_API_KEY)
             try:
-                response = urlopen(quote_plus(api_url, safe="%/:=&?~#+!$,;'@()*[]"))
-            except HTTPError, e:
-                if e.code == 400:
-                    error = 'Error: {}'.format(e.read())
-                else:
-                    error = 'Error: {}'.format(e.code)
-            except URLError, e:
-                error = 'Error: {}'.format(e.reason)
+                # response = urlopen(quote_plus(api_url, safe="%/:=&?~#+!$,;'@()*[]"))
+                response = requests.get(api_url)
+                response.raise_for_status()
+            except HTTPError:
+                error = 'Error: {}'.format(response.status_code)
             else:
-                data = response.read().decode('utf-8').replace('<', '&lt;').replace('>', '&gt;')
+                data = response.text.replace('<', '&lt;').replace('>', '&gt;')
         else:
             error = 'Error: That doesn''t look like a valid Trove url.'
     return render_template('results.html', url=url, data=data, error=error, format=format)
