2fbb941919e35c6a3bbfd555e15220018227cb9e
Tim Sherratt <tim@discontents.com.au>
2020-08-10 16:14:44 +1000

    Refresh and more examples

--- app.py
+++ app.py
@@ -5,17 +5,23 @@ from urllib.parse import unquote_plus, quote_plus
 import re
 from flask import request, render_template
 import os
+from pathlib import Path
+import yaml
+from flask_misaka import Misaka
+from slugify import slugify
 
 TROVE_API_KEY = os.environ['TROVE_API_KEY']
 
 app = Flask(__name__)
-
+md = Misaka()
+md.init_app(app)
 
 @app.route('/', methods=['GET'])
 def show_api_results():
     error = ''
     data = ''
     format = ''
+    examples = yaml.load(Path('examples.yml').read_text())
     url = request.args.get('url', '')
     if url:
         url = unquote_plus(url)
@@ -32,7 +38,11 @@ def show_api_results():
                 data = response.text.replace('<', '&lt;').replace('>', '&gt;')
         else:
             error = 'Error: That doesn''t look like a valid Trove url.'
-    return render_template('results.html', url=url, data=data, error=error, format=format)
+    return render_template('new_results.html', url=url, data=data, error=error, format=format, examples=examples)
+
+@app.template_filter()
+def slugified(value):
+    return slugify(value)
 
 if __name__ == '__main__':
     app.run(debug=True)
